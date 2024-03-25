e7fbeb65e85edf21193fc55af14d05c9688a919c
Srajan Saxena <srajan.somerville@gmail.com>
2024-03-25 23:48:15 +0530

    Set Up Prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
+'use client';
 import Link from 'next/link'
 import React from 'react'
 import { IoBugSharp } from "react-icons/io5";
+import {usePathname} from 'next/navigation'
+import classnames from 'classnames';
 const NavBar = () => {
+    const currentPathname=usePathname();
     const links=[
         {label:'Dashboard',href:'/'},
         {label:'Issues',href:'/issues'}
@@ -10,7 +14,13 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href="/"><IoBugSharp /></Link>
       <ul className='flex space-x-6'>
-        {links.map(link=><Link key={link.href} className='text-zinc-500 hover:text-zinc-900 transition-colors' href={link.href}>
+        {links.map(link=><Link key={link.href} 
+        className={classnames({
+            'text-zinc-900': link.href===currentPathname,
+            'text-zinc-500': link.href!==currentPathname,
+            'hover:text-zinc-800 transition-colors': true
+        })}
+ href={link.href}>
             {link.label}
             </Link>)}
         
